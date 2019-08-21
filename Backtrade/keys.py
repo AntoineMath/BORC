a43e71f816a280be31a510f4c54ea730f75d8bc1
Mathieu <mathieu.cesbron@ecam-rennes.com>
2019-08-21 23:44:35 +0200

    aya

--- Backtrade/keys.py
+++ Backtrade/keys.py
@@ -1,7 +1,2 @@
-from binance.client import Client
-
-
 apiKey = "DKpYeQJDdxgo2mSXIqKnk70QmOy8urDQjJFjPDk06H4waWrj1irLnuFFfHpXMCcC"
 secretKey = "PIp3AKATJfillNs9Lsm1pbUaTMQqMTXFfGS5H77DMwzi6a1GPLOyt78hv03kcf1f"
-
-client = Client(apiKey, secretKey)
