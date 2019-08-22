2aa8139198e9d5d2484b80d02cf0f3e7c88712e8
mathieu cesbron <mathieu.cesbron@ecam-rennes.com>
2019-08-22 20:03:14 +0200

    lower case

--- trade/main.py
+++ trade/main.py
@@ -2,10 +2,9 @@ import sys, os
 from time import sleep
 import datetime
 from binance.client import Client
-
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 import Data.keys
 
 
-client = Client(keys.api_key, keys.secret_key)
+client = Client(Data.keys.apiKey, Data.keys.secretKey)
 print(client)
