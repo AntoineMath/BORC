86b8a9a0c9eaf8214b94db343f002deebabbc37c
mathieu cesbron <mathieu.cesbron@ecam-rennes.com>
2019-08-23 22:13:53 +0200

    logger created

--- trade/simulation.py
+++ trade/simulation.py
@@ -2,12 +2,5 @@ from random import randrange
 import time
 
 
-def __algo_simulation__():
-    while True:
-        yield randrange(3)
-        time.sleep(60)
-
-
-if __name__ == "__main__":
-    for value in __algo_simulation__():
-        print(value)
+def __algo_simulation__(price):
+    return randrange(3)
