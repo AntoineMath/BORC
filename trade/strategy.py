34ddd7929b989614d47d67ba120ac344d3487441
mathieu cesbron <mathieu.cesbron@ecam-rennes.com>
2019-08-24 19:00:00 +0200

    basic trade

--- trade/strategy.py
+++ trade/strategy.py
@@ -11,11 +11,40 @@ from binance.websockets import BinanceSocketManager
 import logging
 
 
-def __trade__(api_key, secret_key):
-    logging.basicConfig(filename="info.log", level=logging.DEBUG)
+def __trade__(api_key, secret_key, pair1, pair2, amount_to_trade):
+    logging.basicConfig(filename="trade.log",
+                        level=logging.INFO,
+                        format='%(asctime)s: %(message)s')
     TIME_START = datetime.datetime.now()
-    CLIENT = Client(api_key, secret_key)
-    logging.info("aya")
+    client = Client(api_key, secret_key)
+    logging.info("START")
+    last_trade = "hodl"
+    start = True
+
+    while True:
+
+        value = float(
+            client.get_order_book(symbol=pair1 + pair2)['bids'][0][0])
+
+        if last_trade == "buy" or start == True:
+
+            if simulation.__algo_simulation__(value) == 0 and float(client.get_asset_balance(asset=pair1)['free']) > amount_to_trade:
+
+                start = False
+                algo_side = "sell"
+                order = client.order_market_sell(symbol=pair1 + pair2, quantity=round(amount_to_trade/value, 4))
+                logging.info("sell BTC to have USDT")
+
+        if  last_trade == "sell" or start == True:
+
+            if simulation.__algo_simulation__(value) == 2 and float(client.get_asset_balance(asset=pair2)['free']) > amount_to_trade:
+
+                start = False
+                algo_side = "buy"
+                order = client.order_market_buy(symbol=pair1 + pair2, quantity=round(amount_to_trade/value, 4))
+                logging.info("buy BTC against USDT")
+
+        sleep(2)
 
 
-print(__trade__(data.keys.apiKey, data.keys.secretKey))
+print(__trade__(data.keys.apiKey, data.keys.secretKey, 'BTC', 'USDT', 3))
