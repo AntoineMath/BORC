109547a3de251463ccaa44688370062f9b2429a1
AntoineMath <antoine.mathu@gmail.com>
2019-08-24 12:42:43 +0200

    changed ranges to numbers
    Multidescrete function needs number of possible states, not a range. So nb = possible states + 1

--- rl/bt_sea_env.py
+++ rl/bt_sea_env.py
@@ -20,7 +20,7 @@ class TradingEnv(gym.Env):
     """
     def __init__(self, train_data, init_invest=20000):
         # data
-        self.stock_price_history = np.around(train_data) # round up to integer to reduce state space
+        self.stock_price_history = np.around(train_data)  # round up to integer to reduce state space
         self.n_stock, self.n_step = self.stock_price_history.shape
 
         # instance attributes
@@ -35,9 +35,9 @@ class TradingEnv(gym.Env):
 
         # observation space: give estimates in order to sample and build scaler
         stock_max_price = self.stock_price_history.max(axis=1)
-        stock_range = [[0, init_invest * 2 // mx] for mx in stock_max_price]
-        price_range = [[0, mx] for mx in stock_max_price]
-        cash_in_hand_range = [[0, init_invest * 2]]
+        stock_range = [init_invest * 2 // mx + 1 for mx in stock_max_price]
+        price_range = [mx + 1 for mx in stock_max_price]
+        cash_in_hand_range = [init_invest * 2 + 1]
         self.observation_space = spaces.MultiDiscrete(stock_range + price_range + cash_in_hand_range)
 
         # seed and start
